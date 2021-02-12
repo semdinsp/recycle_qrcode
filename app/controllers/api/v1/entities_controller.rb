1bd31e6118252c937c399e3770ce087527f286de
semdinsp <scott.sproule@ficonab.com>
2021-02-13 01:14:09 +0800

    minor fixes for checkin

--- app/controllers/api/v1/entities_controller.rb
+++ app/controllers/api/v1/entities_controller.rb
@@ -3,43 +3,41 @@ module Api
     class EntitiesController < ApplicationController
       before_action :set_entity, only: [:show, :checkin ]
       # before_action :authorized
-      
+
 
       # GET /locations
      # curl -H "Content-Type: application/json" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.CCFshmdzekkCKEtOyvOKSGKGHjLM-U_AWS3qySJ9xPA" http://localhost:5000/v1/teams
       def checkin
-        set_activity(params['activity_id'])
-        @metrics=[]
-        @metrics = @activity.metrics.active_scope.order("created_at DESC").all  if !@activity.nil?  #needs only linked to user
-        puts "INDEXS  activity #{@activity.inspect} metrics: #{@metrics.count}"
-        render json: @metrics
+
+        puts "CHECKIN  entity #{@entity.inspect} "
+        render json: @entity
       end
-      
-      
+
+
       def show
-        render json: @metric
+        render json: @entity
       end
-      
+
 
       # DELETE /teams/1
       def destroy
-        set_metric
-        destroy_via_inactive_item(@metric)
+        set_entity(params['id'])
+        destroy_via_inactive_item(@entity)
       end
 
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_entity(act_id)
           # need to limit to only the user involved...
-          @entity = Entity.find(act_id)
+          puts "set enttity params #{params}"
+          @entity = Entity.find(params['id'])
         end
-        
+
         # Only allow a trusted parameter "white list" through.
         def entity_params
           #@activity=set_team(params[:metric][:activity_id]) if !params[:metric][:activity_id].nil?
-          params.require(:entity).permit( :user,  :entity_id )
+          params.require(:entity).permit( :user,  :entity_id, :id )
         end
     end
   end
 end
-
