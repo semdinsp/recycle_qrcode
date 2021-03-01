29969c6621093cc30d66fb514630a869036cb8f6
semdinsp <scott.sproule@ficonab.com>
2021-03-01 20:30:08 +0800

    updat for service test

--- test/services/services_test.rb
+++ test/services/services_test.rb
@@ -8,14 +8,35 @@ class ServicesTest < ActiveSupport::TestCase
   # @entity=entities(:one
   #puts "IN SETUP"
   @location=locations(:one)
-  @entity=entities(:one)
+  @entity=entities(:two)
   puts "SETUP Done"
   end
 
   test "creation" do
       puts "service creation"
-      assert_difference('Actiontype.count', 1) do
+      assert_difference('Actiontype.count', 0) do
         ctx = EntityCheckinAction.execute(entity: @entity, location: @location)
       end
+  end # creation
+
+  test "test empty creation" do
+      puts "service creation"
+      fred=Entity.new
+      fred.save
+      assert_difference('Actiontype.count', 1) do
+        ctx = EntityCheckinAction.execute(entity: fred, location: @location)
+      end
+      assert_difference('Actiontype.count', 0) do
+        # only create one per day
+        ctx = EntityCheckinAction.execute(entity: fred, location: @location)
+      end
+      at=fred.actiontypes.first
+      at.created_at = Time.now - 6.days
+      at.save
+      assert_difference('Actiontype.count', 1) do
+        # only create one per day
+        ctx = EntityCheckinAction.execute(entity: fred, location: @location)
+      end
+
   end # creation
 end
