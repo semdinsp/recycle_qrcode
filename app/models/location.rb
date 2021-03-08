ec1fd7e02c92fffd1fad7f72dae23cacffac1165
semdinsp <scott.sproule@ficonab.com>
2021-03-08 14:11:01 +0800

    update for checkin code

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ApplicationRecord
   #belongs_to :entity  SCOTT CHECK
   belongs_to :locatable, polymorphic: true
   # convert old location stuff to locatable   ... Location.all.each { |l| e=Entity.find(l.entity_id); e.location=l; e.save; l.save }
+
+def latLng
+  return "#{self.latitude} : #{self.longitude}"
+end
 end
