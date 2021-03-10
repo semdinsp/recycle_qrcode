7d379a18ed1251a7de0e576e8e9a12ba3d01b5c9
semdinsp <scott.sproule@ficonab.com>
2021-03-10 17:50:49 +0800

    update for deployment fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Setting.first.nil?
+  puts "creating new settings"
+  setting=Setting.new
+  setting.host="https://recycle-qrcode.herokuapp.com"
+  setting.port=""
+  setting.centerlat= -8.5569
+  setting.centerlong= 125.5603
+  setting.zoom=12
+  setting.save
+  puts "settings now #{setting.inspect}"
+else
+  puts "Settings already exist:  #{Setting.first.inspect}"
+end
