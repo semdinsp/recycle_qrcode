9da2ad4ca4081eef379c743db2e6be7fbba2ed56
semdinsp <scott.sproule@ficonab.com>
2021-03-10 18:14:00 +0800

    addded setting varialbe

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-#SETTINGS=Setting.first
-#puts "System settings:"
-# puts SETTINGS.inspect if !Setting.first.nil?
+SETTINGS=Setting.first
+puts "System settings:"
+puts SETTINGS.inspect if !Setting.first.nil?
